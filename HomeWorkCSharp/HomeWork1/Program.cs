c0fe58a8d2b31041417ce103c3fe0963c264c603
Алексей Шербаков <leha.gb.2023@mail.ru>
2024-06-12 17:44:21 +1000

    HomeWork 1

---
+++ HomeWorkCSharp/HomeWork1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!!");
+            Console.ReadKey();
+
+            Console.Write("Hello ");
+            Console.Write("World");
+            Console.Write("!!! ");
+            Console.ReadLine();
+        }
+    }
+}
