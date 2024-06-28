b31365d2428c162ff768bee7a0b269d24a152f27
Алексей Шербаков <leha.gb.2023@mail.ru>
2024-06-29 06:43:50 +1000

    Lecture 6.1 Встоенные методы.Класс Math, Convert, DateTime

---
+++ LectureCSharp/Lecture6/Lecture6.1/Example1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture6._1
+{
+    // 6.1 Встроенные методы. Класс Math и Convert, DateTime
+    class Example1
+    {
+        // Класс Math
+        static void Main(string[] args)
+        {
+            // Возвращает наименьшее целое число, котрое больше и равно заданному десятичному.
+            Console.WriteLine(Math.Ceiling(1.2));  // 2
+            Console.WriteLine(Math.Ceiling(-1.2));  // -1
+
+
+            // Возвращает значение е, возведенное в указаную степень.
+            Console.WriteLine(Math.Exp(2));  //e * e
+
+
+            // Возвращает наибольшее целое число, котрое больше и равно заданному десятичному числу.
+            Console.WriteLine(Math.Floor(1.2));  // 1
+            Console.WriteLine(Math.Floor(-1.2));  // -2
+
+
+            // Возвращает указанное число, возведенное в укзанную степень.
+            Console.WriteLine(Math.Pow(4, 3));  // 4 * 4 * 
+            Console.WriteLine(Math.Pow(2022, 2));  // 2022 * 2022
+
+
+            // Округляет десятичное значение до указанного числа дробного разряда.
+            Console.WriteLine(Math.Round(1.2345));    // 1
+            Console.WriteLine(Math.Round(1.5345));    // 2
+            Console.WriteLine(Math.Round(-1.5345));   // -2
+            Console.WriteLine(Math.Round(-1.56789, 3));  // -1.568
+
+
+            // Возвращает целое число, указывающее знак 8-разрядного целого числа со знаком.
+            Console.WriteLine(Math.Sign(20));  // 1
+            Console.WriteLine(Math.Sign(-20));  // -1
+
+            int r = 2;
+            double s = Math.PI * Math.Pow(r, 2);
+            Console.WriteLine($"double s = {Math.Round(s, 3)}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LectureCSharp/Lecture6/Lecture6.1/Example2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture6._1
+{
+    // DataTime, TimeSpan
+
+    class Example2
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = new DateTime(2024, 06, 28, 20, 11, 00);
+            Console.WriteLine($"{date}");
+
+
+            Console.WriteLine(date.ToShortTimeString());   // Получение только времени
+            Console.WriteLine(date.ToShortDateString());   // Получение только даты
+
+
+            DateTime newDate = date.AddDays(10);
+            Console.WriteLine($"{newDate}");
+
+
+            TimeSpan span = newDate - date;
+            Console.WriteLine($"{span}");
+            Console.WriteLine($"{span.TotalMinutes}");
+            Console.WriteLine($"{span.TotalDays}");
+
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("===========================================");
+
+            // Запоминаем текущее время
+            date = DateTime.Now;
+
+            double sum = 0;
+
+            for (int i = 0; i <= 1_000_000; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine($"sum = {sum}");
+
+
+            // Вычитаем из текущего времени, запомнившее время 
+            // и выводим результат работы прораммы в милисекундах
+            TimeSpan timeSpan = DateTime.Now.Subtract(date);
+            Console.WriteLine($"timeSpan.TotalMilliseconds = {timeSpan.TotalMilliseconds}");
+            Console.ReadKey();
+        }
+    }
+}
