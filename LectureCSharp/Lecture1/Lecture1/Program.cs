f9f78ba9a11288e47642dcd23df69b4e3fc2ba6e
Алексей Шербаков <leha.gb.2023@mail.ru>
2024-06-29 14:45:07 +1000

    Lecture 1: Введение в профессию .NET Developer.

---
+++ LectureCSharp/Lecture1/Lecture1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture1
+{
+
+    // Введение в профессию .NET Developer.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
