84249f42bedae00dd6bd8f3414149ab7d1500ff8
Алексей Шербаков <leha.gb.2023@mail.ru>
2024-07-01 08:57:10 +1000

    Lecture 6.5: Расширенная работа со строками: StringReader и StringWriter; изменяемые строки: StringBuilder

---
+++ LectureCSharp/Lecture6/Lecture6.5/Example1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture6._5
+{
+
+    // 6.5 Расширенная работа со строками: StringReader и StringWriter; изменяемые строки: StringBuilder
+
+    class Example1
+    {
+        static void Main(string[] args)
+        {
+            // Проблема string
+            string s1 = "Првет";
+            string s2 = s1.Insert(2, "и");
+            Console.WriteLine($"s1 = {s1}, s2 = {s2}");
+            Console.WriteLine("Объем выделенной памяти");
+            Console.WriteLine($"s1 = {s1.Length * 2} байт, s2 = {s2.Length * 2} байт");
+
+            Console.WriteLine("====================================");
+
+            // StringBuilder // Представляет изменяемую строку символов.
+            StringBuilder sb1 = new StringBuilder("Првет");
+            Console.WriteLine($"\nДлина sb1 = {sb1.Length}");
+            Console.WriteLine($"Вместимость sb1 = {sb1.Capacity}");
+
+            Console.WriteLine("====================================");
+
+            StringBuilder sb2 = new StringBuilder(100);
+            Console.WriteLine($"\nДлина sb2 = {sb2.Length}");
+            Console.WriteLine($"Вместимость sb2 = {sb2.Capacity}");
+            Console.WriteLine("====================================");
+
+            // Добавляет копию строки к данному экземпляру.
+            sb2.Append("Один");
+            Console.WriteLine($"\nsb2 = {sb2}, sb2.Length = { sb2.Length}, sb2.Capacity = {sb2.Capacity}");
+            Console.WriteLine("====================================");
+
+            // Добавляет к данному экземпляру строку, возвращаемую в результате обработки
+            // строки составного формата.
+            sb2.AppendFormat("{0} {1}", "Два", " Три");
+            Console.WriteLine($"\nsb2 = {sb2}, sb2.Length = { sb2.Length}, sb2.Capacity = {sb2.Capacity}");
+            Console.WriteLine("====================================");
+
+            // Добавляет знак завершения строки по умолчанию в конец.
+            sb2.AppendLine(".");
+            Console.WriteLine($"\nsb2 = {sb2}, sb2.Length = { sb2.Length}, sb2.Capacity = {sb2.Capacity}");
+            Console.WriteLine("====================================");
+
+            // Добавляет копию указанной строки, и знак завершения строки по умолчанию в конец
+            // текущего объекта.
+            sb2.AppendLine("Четыре");
+            Console.WriteLine($"\nsb2 = {sb2}, sb2.Length = { sb2.Length}, sb2.Capacity = {sb2.Capacity}");
+            Console.WriteLine("====================================");
+
+            // Удаляет указанный диапазон символов из данного экземпляра.
+            sb2.Remove(14, 4);
+            Console.WriteLine($"После удаления sb2 = {sb2}");
+            Console.WriteLine("====================================");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LectureCSharp/Lecture6/Lecture6.5/Example2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lecture6._5
+{
+
+    // 6.5 Расширенная работа со строками: StringReader и StringWriter; изменяемые строки: StringBuilder
+
+    class Example2
+    {
+        static void Main(string[] args)
+        {
+            // using System.IO;
+            // Реализует объект System.IO.TextWriter для записи сведений в строку.
+            // Сведения хранятся в базовом System.Text.StringBuilder.
+
+            StringBuilder sb = new StringBuilder(100);
+            string s = "Ролик 5. Расширенная работа со строками: StringReader и StringWriter;" +
+                "изменяемые строки: StringBuilder";
+            sb.Append(s);
+            Console.WriteLine($"{sb}");
+
+            using(StringWriter sw = new StringWriter(sb))
+            {
+                sw.Write("!!!");
+            }
+            Console.WriteLine($"{sb}");
+
+            // Реализует System.IO.StringReader считывает данные из строки.
+            using(StringReader sr = new StringReader(s))
+            {
+                char[] buf = new char[s.Length];
+                sr.ReadBlock(buf, 0, buf.Length);
+                foreach (var e in buf)
+                {
+                    Console.Write($"{e}");
+                }
+            }
+            Console.ReadKey();
+           
+        }
+    }
+}
