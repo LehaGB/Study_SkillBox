094f1ef1b02298da8b4c7f821706e8db01b7ed38
Алексей Шербаков <leha.gb.2023@mail.ru>
2024-09-30 08:49:30 +1000

    Lecture № 9, WPF

---
+++ LectureCSharp/Lecture9/Lecture9.3/Worker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture9._3
+{
+    public struct Worker
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public int Salaary { get; set; }
+
+        //public override string ToString()
+        //{
+        //    return $"{Name,20} {Age,4} {Salaary,10}";
+        //}
+    }
+}
